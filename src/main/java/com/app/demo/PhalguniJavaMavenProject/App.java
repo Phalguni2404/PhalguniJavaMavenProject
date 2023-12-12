d07e884e653aa35e2e7820e75b5d0de19e9f86de
Phalguni <pallaphalguni2404@gmail.com>
2023-12-12 23:18:34 +0530

    Demo for team

---
+++ src/main/java/com/app/demo/PhalguniJavaMavenProject/App.java
@@ -0,0 +1,14 @@
+package com.app.demo.PhalguniJavaMavenProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World from Phalguni!" );
+        System.out.println( "This is SE lab practical" );
+    }
+}
